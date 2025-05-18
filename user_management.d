79aee7d8e49eba7d67ca4c85c9aa62ea0d21097e
Emkei <152603256+Emkei69@users.noreply.github.com>
2025-05-19 02:16:36 +0300

    Create user_management.d

---
+++ user_management.d
@@ -0,0 +1,17 @@
+# Linux commands for user management
+
+- Create a new user: `adduser <name>`
+- Change the parameters of an existing user: `usermod <parameters>`
+- Delete user: `deluser <name>`
+- Change the user's password: `passwd <name>`
+- Switch to another user or become a superuser: `su <user>`
+- Run a command with superuser privileges: `sudo <command>`
+- Display user information: `finger <name>`
+- Display information about logged-in users: `who`
+- Display information about the current or specified user: `id <name>`
+- Display the groups to which the user belongs: `groups <name>`
+- Create a new user (alternative for `adduser'): `useradd <name>`
+- Delete the user (alternative for `deluser'): `userdel <name>`
+- Display user login history: `last`
+- Show current users and their activity: `w`
+- Log out of the current user session: `logout`
